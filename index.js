c71f050aba68aa82e407d08eca90bdeac28219bf
su-de-sh <sudesh7443@gmail.com>
2022-09-16 22:47:21 +0545

    chore: simulate an update

--- index.js
+++ index.js
@@ -17,8 +17,6 @@ morgan(function (tokens, req, res) {
     tokens["response-time"](req, res),
     "ms",
   ].join(" ");
-
-  console.log(log);
 });
 
 morgan.token("body", (req) => JSON.stringify(req.body));
